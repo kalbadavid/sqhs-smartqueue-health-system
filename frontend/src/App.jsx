97aef810b88114dfb5008de03ab582b70972dc6d
kalba david <mckalbadavid@gmail.com>
2026-04-24 22:44:04 +0100

    Initial commit for SQHS SmartQueue Health System

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PredictWaitTime from './components/PredictWaitTime';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <PredictWaitTime />
+    </div>
+  );
+}
+
+export default App;
